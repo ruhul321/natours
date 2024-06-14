4e46bf465d9737adc0d894a27d398408355679fa
Ruhul <ruhulbarbhuiya228@gmail.com>
2024-06-14 23:48:44 +0530

    updated server.js

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ mongoose
   .then(() => console.log('DB connection successfull'));
 // console.log(process.env);
 
-const port = 3000; //process.env.PORT ||
+const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
   console.log(`App running on port ${port}`);
 });
